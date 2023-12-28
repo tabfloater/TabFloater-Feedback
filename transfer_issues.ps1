886a433d8ffced33b310216b2a33f45c70171c85
Justin <jusstewa@gmail.com>
2023-12-28 10:47:02 -0600

    Remove unnecessary files and scripts

--- transfer_issues.ps1
+++ transfer_issues.ps1
@@ -1,12 +1,21 @@
 # Fetch issues from the old repository
-$oldIssues = Invoke-RestMethod -Uri 'https://api.github.com/repos/tabfloater/tabfloater/issues' -Headers @{ "Authorization" = "Bearer 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" }
+$oldIssues = Invoke-RestMethod -Uri 'https://api.github.com/repos/tabfloater/tabfloater/issues' -Headers @{ "Authorization" = "Bearer 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" }
 
 # Create issues in the new repository
 foreach ($issue in $oldIssues) {
     $body = @{
         title = $issue.title
-        body = $issue.body
+        body = @"
+Originally posted by @$($issue.user.login) in the old repository:
+
+$($issue.body)
+
+---
+- Issue created on: $($issue.created_at)
+- Number of comments: $($issue.comments)
+- Labels: $($issue.labels | ForEach-Object { $_.name }) 
+"@
     } | ConvertTo-Json
 
-    Invoke-RestMethod -Method Post -Uri 'https://api.github.com/repos/tabfloater/TabFloater-Feedback/issues' -Headers @{ "Authorization" = "Bearer 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" } -Body $body
+    Invoke-RestMethod -Method Post -Uri 'https://api.github.com/repos/tabfloater/TabFloater-Feedback/issues' -Headers @{ "Authorization" = "Bearer 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" } -Body $body
 }
